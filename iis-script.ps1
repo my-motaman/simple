3942cea944849117ce350d2c5e1e4c265aa83769
my-motaman <55682597+my-motaman@users.noreply.github.com>
2019-09-26 17:42:30 +0800

    Update iis-script.ps1

--- iis-script.ps1
+++ iis-script.ps1
@@ -1,3 +1,17 @@
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 Install-WindowsFeature Web-Asp-Net45
 Install-WindowsFeature -Name Web-Mgmt-Service
+
+Enable-PSRemoting
+
+New-SMBShare –Name "Shared" –Path "D:\" –FullAccess "Everyone"
+
+$url = "https://go.microsoft.com/fwlink/?LinkId=287166"
+$output = "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi"
+$start_time = Get-Date
+
+Invoke-WebRequest -Uri $url -OutFile $output
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+C:\WebPlatformInstaller_amd64_en-US.msi /quiet
+
+&"C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd.exe" /Install /Products:WDeploy /AcceptEula

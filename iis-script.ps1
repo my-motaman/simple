64f1e00fd74f65cc2a1ac4097533d2bd7584fd82
my-motaman <55682597+my-motaman@users.noreply.github.com>
2019-09-30 00:15:33 +0800

    Update iis-script.ps1

--- iis-script.ps1
+++ iis-script.ps1
@@ -1,3 +1,11 @@
+param
+(
+    [string] $hostname,
+    [string] $protocol
+)
+
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 Install-WindowsFeature Web-Asp-Net45
 Install-WindowsFeature -Name Web-Mgmt-Service
+
+& ./config-script.ps1 $hostname $protocol

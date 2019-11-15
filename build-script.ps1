b5f497ed900bec4cd618ec6f5fd55a8f9ed7b1bc
my-motaman <55682597+my-motaman@users.noreply.github.com>
2019-11-16 00:22:37 +0800

    Update build-script.ps1

--- build-script.ps1
+++ build-script.ps1
@@ -28,18 +28,19 @@ choco install azure-cli -y -v
 $agentPool = "zz-it-pool"
 $agentNamePrefix = "$env:computername-Agent-"
 $agentSource = "$HOME\Downloads\vsts-agent-win-x64-2.158.0.zip"
-$vstsUrl = "https://dev.azure.com/symphonyvsts"
-$vstsToken = "rtnikf4zsseahkpw6ek4jbglip3z57z67u4qvmztk3divxgt2jnq"
+$vstsUrl = "https://dev.azure.com/symphonyvsts-training"
+$vstsToken = "n7m52vakh5dsqogzko34ompwy723vx3grvbqpet3g33ax2mu5gyq"
 $agentUrl = "https://vstsagentpackage.azureedge.net/agent/2.158.0/vsts-agent-win-x64-2.158.0.zip"
 $agentFilename = Split-Path $agentUrl -leaf
 $agentSource = "$HOME\Downloads\$agentFilename"
+$agentDestination = "C:\Work"
 Write-Host $agentSource
 
 Invoke-WebRequest -Uri "$agentUrl" -OutFile "$agentSource"
 
 for($i=1; $i -le 1; $i++) {  
 $agentName = "$agentNamePrefix$i"
-Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$agentSource", "$(agentDestination)\$agentName")
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$agentSource", "$agentDestination\$agentName")
 &"$agentDestination\$agentName\config.cmd" --unattended --url "$vstsUrl" --auth pat --token $vstsToken --pool $agentPool --agent $agentName --runAsService
 Get-CimInstance win32_service -filter "name='vstsagent.symphonyvsts.$agentPool.$agentName'" | Invoke-CimMethod -Name Change -Arguments @{StartName="LocalSystem"}	
 } 
